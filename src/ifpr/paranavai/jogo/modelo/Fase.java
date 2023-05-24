4b57f61a8e33b746d90d27c8f4f3911bc92ea028
Marcelo F. Terenciani <marcelo.terenciani@gmail.com>
2023-05-24 16:50:59 -0300

    Capítulo 7 - Personagem

--- src/ifpr/paranavai/jogo/modelo/Fase.java
+++ src/ifpr/paranavai/jogo/modelo/Fase.java
@@ -9,15 +9,20 @@ import javax.swing.JPanel;
 
 public class Fase extends JPanel {
     private Image fundo;
+    private Personagem personagem;
 
     public Fase() {
         ImageIcon carregando = new ImageIcon("recursos\\fundo.jpg");
         fundo = carregando.getImage();
+
+        personagem = new Personagem();
+        personagem.carregar();
     }
 
     public void paint(Graphics g) {
         Graphics2D graficos = (Graphics2D) g;
         graficos.drawImage(fundo, 0, 0, null);
+        graficos.drawImage(personagem.getImagem(), personagem.getPosicaoEmX(), personagem.getPosicaoEmY(), this);
         g.dispose();
     }
 }
---
+++ src/ifpr/paranavai/jogo/modelo/Personagem.java
@@ -0,0 +1,84 @@
+package ifpr.paranavai.jogo.modelo;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+public class Personagem {
+    private int posicaoEmX;
+    private int posicaoEmY;
+    private int deslocamentoEmX;
+    private int deslocamentoEmY;
+    private Image imagem;
+    private int larguraImagem;
+    private int alturaImagem;
+
+    public Personagem() {
+        this.posicaoEmX = 100;
+        this.posicaoEmY = 100;
+    }
+
+    public void carregar() {
+        ImageIcon carregando = new ImageIcon("recursos\\espaconave.png");
+        this.imagem = carregando.getImage();
+        this.alturaImagem = this.imagem.getWidth(null);
+        this.larguraImagem = this.imagem.getHeight(null);
+    }
+
+    public int getPosicaoEmX() {
+        return this.posicaoEmX;
+    }
+
+    public void setPosicaoEmX(int posicaoEmX) {
+        this.posicaoEmX = posicaoEmX;
+    }
+
+    public int getPosicaoEmY() {
+        return this.posicaoEmY;
+    }
+
+    public void setPosicaoEmY(int posicaoEmY) {
+        this.posicaoEmY = posicaoEmY;
+    }
+
+    public int getDeslocamentoEmX() {
+        return this.deslocamentoEmX;
+    }
+
+    public void setDeslocamentoEmX(int deslocamentoEmX) {
+        this.deslocamentoEmX = deslocamentoEmX;
+    }
+
+    public int getDeslocamentoEmY() {
+        return this.deslocamentoEmY;
+    }
+
+    public void setDeslocamentoEmY(int deslocamentoEmY) {
+        this.deslocamentoEmY = deslocamentoEmY;
+    }
+
+    public Image getImagem() {
+        return this.imagem;
+    }
+
+    public void setImagem(Image imagem) {
+        this.imagem = imagem;
+    }
+
+    public int getLarguraImagem() {
+        return this.larguraImagem;
+    }
+
+    public void setLarguraImagem(int larguraImagem) {
+        this.larguraImagem = larguraImagem;
+    }
+
+    public int getAlturaImagem() {
+        return this.alturaImagem;
+    }
+
+    public void setAlturaImagem(int alturaImagem) {
+        this.alturaImagem = alturaImagem;
+    }
+
+}
