16b9061c7125960fe77d250f901d1864e8ec37cc
Marcelo F. Terenciani <marcelo.terenciani@gmail.com>
2023-08-21 12:08:25 -0300

    Capítulo 22: Criação da classe Asteroide

---
+++ src/ifpr/paranavai/jogo/modelo/Asteroide.java
@@ -0,0 +1,33 @@
+package ifpr.paranavai.jogo.modelo;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+import ifpr.paranavai.jogo.principal.Principal;
+
+public class Asteroide extends ElementoGrafico {
+    private static int VELOCIDADE = 1;
+
+    public Asteroide(int xAleatorio, int yAleatorio) {
+        this.carregar();
+        super.setPosicaoEmX(xAleatorio);
+        super.setPosicaoEmY(yAleatorio);
+    }
+
+    public void carregar() {
+        ImageIcon carregando = new ImageIcon("recursos\\asteroide.png");
+        super.setImagem(carregando.getImage());
+    }
+
+    @Override
+    public void atualizar() {
+        if (this.getPosicaoEmX() < 0) {
+            int x = (int) (Math.random() * Principal.LARGURA_DA_JANELA);
+            int y = (int) (Math.random() * Principal.ALTURA_DA_JANELA);
+            super.setPosicaoEmX(x);
+            super.setPosicaoEmX(y);
+        } else {
+            super.setPosicaoEmX(super.getPosicaoEmX() - VELOCIDADE);
+        }
+    }
+}
